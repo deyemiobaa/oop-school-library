c87503be6136ad56e5936bd4868c043d374be975
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-06-24 08:40:52 +0100

    remove comments

--- person.rb
+++ person.rb
@@ -60,9 +60,3 @@ class TrimmerDecorator < Decorator
   end
 end
 
-# person = Person.new(22, 'maximilianus')
-# person.correct_name
-# capitalizedPerson = CapitalizeDecorator.new(person)
-# capitalizedPerson.correct_name
-# capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-# capitalizedTrimmedPerson.correct_name
