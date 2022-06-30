2f61aad72e05107a9a38c1af7524572fe21a1174
BisiAlfred <kalumunabisimwa5@gmail.com>
2022-06-30 16:37:01 +0200

    test decorator and rental

--- spec/decorator_spec.rb
+++ spec/decorator_spec.rb
@@ -1,1 +1,19 @@
 require '../decorator'
+
+describe Decorator do
+  it 'has a name' do
+    decorator = Decorator.new('John')
+    expect(decorator).to have_attributes(nameable: 'John')
+  end
+end
+
+describe CapitalizeDecorator do
+  it 'Capitalizes name' do
+    expect(CapitalizeDecorator.new(Decorator.mew('John'))).to have_attributes(nameable: 'John')
+  end
+end
+describe TrimmerDecorator do
+  it 'Trims name' do
+    expect(TrimmerDecorator.new(Decorator.mew('John'))).to have_attributes(nameable: 'John')
+  end
+end
--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -1,1 +1,10 @@
 require '../rental'
+
+describe Rental do
+  it 'is an instance of Rental' do
+    student = double('Student')
+    book = double('Book')
+    rental = Rental.new('2022/09/10', student, book)
+    expect(rental).to be_instance_of(Rental)
+  end
+end
