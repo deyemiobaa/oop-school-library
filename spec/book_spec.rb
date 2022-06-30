77f02c13a7d2c4b3cb95aec15621259d14f04991
BisiAlfred <kalumunabisimwa5@gmail.com>
2022-06-30 16:40:15 +0200

    testing book

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,1 +1,12 @@
 require '../book'
+
+describe Book do
+  it 'has a title' do
+    book = Book.new('The Hobbit', 'Author')
+    expect(book.title).to eq('The Hobbit', 'Author')
+  end
+  it 'has a author' do
+    book = Book.new('The Hobbit', 'Author')
+    expect(book.author).to eq('Author')
+  end
+end
